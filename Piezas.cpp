a190c1f24f96323f77a7a17a74caba1cc54ea2a4
Jaskaal <cadams39@mail.csuchico.edu>
2020-05-08 17:25:09 -0700

    Fix spc/tabs formatting in Piezas.cpp, attempt dropPiece()

--- Piezas.cpp
+++ Piezas.cpp
@@ -1,76 +1,99 @@
 #include "Piezas.h"
 #include <vector>
 /** CLASS Piezas
- * Class for representing a Piezas vertical board, which is roughly based
- * on the game "Connect Four" where pieces are placed in a column and 
- * fall to the bottom of the column, or on top of other pieces already in
- * that column. For an illustration of the board, see:
- *  https://en.wikipedia.org/wiki/Connect_Four
- *
- * Board coordinates [row,col] should match with:
- * [2,0][2,1][2,2][2,3]
- * [1,0][1,1][1,2][1,3]
- * [0,0][0,1][0,2][0,3]
- * So that a piece dropped in column 2 should take [0,2] and the next one
- * dropped in column 2 should take [1,2].
+* Class for representing a Piezas vertical board, which is roughly based
+* on the game "Connect Four" where pieces are placed in a column and 
+* fall to the bottom of the column, or on top of other pieces already in
+* that column. For an illustration of the board, see:
+*  https://en.wikipedia.org/wiki/Connect_Four
+*
+* Board coordinates [row,col] should match with:
+* [2,0][2,1][2,2][2,3]
+* [1,0][1,1][1,2][1,3]
+* [0,0][0,1][0,2][0,3]
+* So that a piece dropped in column 2 should take [0,2] and the next one
+* dropped in column 2 should take [1,2].
 **/
 
 
 /**
- * Constructor sets an empty board (default 3 rows, 4 columns) and 
- * specifies it is X's turn first
+* Constructor sets an empty board (default 3 rows, 4 columns) and 
+* specifies it is X's turn first
 **/
 Piezas::Piezas() {
-	// Resize base of gameboard to have valid row amount
-	board.resize(3);
+// Resize base of gameboard to have valid row amount
+board.resize(3);
 
-	// For every row, resize row to proper size
-	for(int i = 0; i < board.size(); board[i].resize(4), i++) {
-		
-		// For every row[i] element, reset
-		for(int j = 0; j < board[i].size(); j++) {
-			board[i][j] = Blank;
-		}
+// For every row, resize row to proper size
+for(int i = 0; i < board.size(); board[i].resize(4), i++) {
+	
+	// For every row[i] element, reset
+	for(int j = 0; j < board[i].size(); j++) {
+		board[i][j] = Blank;
 	}
-	turn = X;
+}
+turn = X;
 }
 
 /**
- * Resets each board location to the Blank Piece value, with a board of the
- * same size as previously specified
+* Resets each board location to the Blank Piece value, with a board of the
+* same size as previously specified
 **/
+
 void Piezas::reset() {
+// For every row, resize row to proper size
+for(int i = 0; i < board.size(); board[i].resize(4), i++) {
+	
+	// For every row[i] element, reset
+	for(int j = 0; j < board[i].size(); j++) {
+		board[i][j] = Blank;
+	}
+}
 }
 
 /**
- * Places a piece of the current turn on the board, returns what
- * piece is placed, and toggles which Piece's turn it is. dropPiece does 
- * NOT allow to place a piece in a location where a column is full.
- * In that case, placePiece returns Piece Blank value 
- * Out of bounds coordinates return the Piece Invalid value
- * Trying to drop a piece where it cannot be placed loses the player's turn
+* Places a piece of the current turn on the board, returns what
+* piece is placed, and toggles which Piece's turn it is. dropPiece does 
+* NOT allow to place a piece in a location where a column is full.
+* In that case, placePiece returns Piece Blank value 
+* Out of bounds coordinates return the Piece Invalid value
+* Trying to drop a piece where it cannot be placed loses the player's turn
 **/ 
 Piece Piezas::dropPiece(int column) {
-    return Blank;
+	Piece tmpTurn = turn;
+
+	// Toggle turn
+	if(turn == X) {
+		turn O;
+	} else {
+		turn = X;
+	}
+
+	// OOB checking on int column
+	if(column >= /* column size/stat int 4 */) {return Invalid;}
+	for(int i = 0; i < board.size(); i++) {
+		
+	}
+	return Blank;
 }
 
 /**
- * Returns what piece is at the provided coordinates, or Blank if there
- * are no pieces there, or Invalid if the coordinates are out of bounds
+* Returns what piece is at the provided coordinates, or Blank if there
+* are no pieces there, or Invalid if the coordinates are out of bounds
 **/
 Piece Piezas::pieceAt(int row, int column) {
-    return Blank;
+	return Blank;
 }
 
 /**
- * Returns which Piece has won, if there is a winner, Invalid if the game
- * is not over, or Blank if the board is filled and no one has won ("tie").
- * For a game to be over, all locations on the board must be filled with X's 
- * and O's (i.e. no remaining Blank spaces). The winner is which player has
- * the most adjacent pieces in a single line. Lines can go either vertically
- * or horizontally. If both X's and O's have the same max number of pieces in a
- * line, it is a tie.
+* Returns which Piece has won, if there is a winner, Invalid if the game
+* is not over, or Blank if the board is filled and no one has won ("tie").
+* For a game to be over, all locations on the board must be filled with X's 
+* and O's (i.e. no remaining Blank spaces). The winner is which player has
+* the most adjacent pieces in a single line. Lines can go either vertically
+* or horizontally. If both X's and O's have the same max number of pieces in a
+* line, it is a tie.
 **/
 Piece Piezas::gameState() {
-    return Blank;
+	return Blank;
 }
